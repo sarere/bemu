242742582b2e767d65bac832e9431e68c0da8ab8
rere <rere@prismapp.io>
2017-04-21 01:57:31 +0700

    add content and sub menu

--- resources/views/profil/profil.blade.php
+++ resources/views/profil/profil.blade.php
@@ -6,12 +6,141 @@
 <div class="col-md-12">
     asd
 </div>
-<div class="col-md-10 col-md-offset-1">
-    <div class="col-md-9 border">
-        <h1 class="primary-color">Profil</h1>
+<div class="col-md-8 col-md-offset-1">
+    <div class="col-md-12">
+        <div id="section-visi">
+            <div class="profile-title">
+                <a class="glyphicon glyphicon-link link-ico jumper" href="#section-visi"></a>
+                <h1 class="primary-color display-inline-block">Visi</h1>
+            </div>
+            <div class="profile-desc">
+                Visi kami
+            </div>
+        </div>
+        <div id="section-misi">
+            <div class="profile-title">
+                <a class="glyphicon glyphicon-link link-ico jumper" href="#section-misi"></a>
+                <h1 class="primary-color display-inline-block">Misi</h1>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+        </div>
+        <div id="section-struktur-org">
+            <div class="profile-title">
+                <a class="glyphicon glyphicon-link link-ico jumper" href="#section-struktur-org"></a>
+                <h1 class="primary-color display-inline-block">Struktur Organisasi</h1>
+            </div>
+            <div id="section-bagan-struktur">
+                <div class="profile-sub-title">
+                    <a class="glyphicon glyphicon-link link-ico jumper" href="#section-bagan-struktur"></a>
+                    <h2 class="primary-color display-inline-block margin-top-tiny">Bagan Struktur Organisasi BEMU</h2>
+                </div>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+            <div id="section-presiden-univ">
+                <div class="profile-sub-title">
+                    <a class="glyphicon glyphicon-link link-ico jumper" href="#section-presiden-univ"></a>
+                    <h2 class="primary-color display-inline-block margin-top-tiny">Presiden Universitas</h2>
+                </div>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+            <div id="section-wakil-presiden-univ">
+                <div class="profile-sub-title">
+                    <a class="glyphicon glyphicon-link link-ico jumper" href="#section-wakil-presiden-univ"></a>
+                    <h2 class="primary-color display-inline-block margin-top-tiny">Wakil Presiden Universitas</h2>
+                </div>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+            <div id="section-kementrian-kesekretariatan">
+                <div class="profile-sub-title">
+                    <a class="glyphicon glyphicon-link link-ico jumper" href="#section-kementrian-kesekretariatan"></a>
+                    <h2 class="primary-color display-inline-block margin-top-tiny">Kementrian Kesekretariatan</h2>
+                </div>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+            <div id="section-kementrian-keuangan">
+                <div class="profile-sub-title">
+                    <a class="glyphicon glyphicon-link link-ico jumper" href="#section-kementrian-keuangan"></a>
+                    <h2 class="primary-color display-inline-block margin-top-tiny">Kementrian Keuangan</h2>
+                </div>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+            <div id="section-kementrian-luar-negeri">
+                <div class="profile-sub-title">
+                    <a class="glyphicon glyphicon-link link-ico jumper" href="#section-kementrian-luar-negeri"></a>
+                    <h2 class="primary-color display-inline-block margin-top-tiny">Kementrian Luar Negeri</h2>
+                </div>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+            <div id="section-kementrian-dalam-negeri">
+                <div class="profile-sub-title">
+                    <a class="glyphicon glyphicon-link link-ico jumper" href="#section-kementrian-dalam negeri"></a>
+                    <h2 class="primary-color display-inline-block margin-top-tiny">Kementrian Dalam Negeri</h2>
+                </div>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+            <div id="section-kementrian-olahraga">
+                <div class="profile-sub-title">
+                    <a class="glyphicon glyphicon-link link-ico jumper" href="#section-kementrian-olahraga"></a>
+                    <h2 class="primary-color display-inline-block margin-top-tiny">Kementrian Olahraga</h2>
+                </div>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+            <div id="section-kementrian-sosebud">
+                <div class="profile-sub-title">
+                    <a class="glyphicon glyphicon-link link-ico jumper" href="#section-kementrian-sosebud"></a>
+                    <h2 class="primary-color display-inline-block margin-top-tiny">Kementrian Sosial Seni & Budaya</h2>
+                </div>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+            <div id="section-kementrian-ti">
+                <div class="profile-sub-title">
+                    <a class="glyphicon glyphicon-link link-ico jumper" href="#section-kementrian-ti"></a>
+                    <h2 class="primary-color display-inline-block margin-top-tiny">Kementrian Teknik Informasi</h2>
+                </div>
+            </div>
+            <div class="profile-desc">
+                asd
+            </div>
+        </div>
     </div>
-    <div class="col-md-3 border">
-        <h1 class="primary-color">Menu</h1>
+</div>
+<div class="col-md-2 sub-nav pad-top">
+    <div class="sub-nav-outer">
+        <a href="#section-visi" class="jumper">Visi</a>
+        <a href="#section-misi" class="jumper">Misi</a>
+        <a href="#section-struktur-org" class="jumper">Struktur Organisasi BEMU</a>
+        <a href="#section-bagan-struktur" class="jumper">Bagan Struktur Organisasi BEMU</a>
+    </div>
+    <div class="sub-nav-inner">
+        <a href="#section-presiden-univ" class="jumper">Presiden Universitas</a>
+        <a href="#section-wakil-presiden-univ" class="jumper">Wakil Presiden Universitas</a>
+        <a href="#section-kementrian-kesekretariatan" class="jumper">Kementrian Kesekretariatan</a>
+        <a href="#section-kementrian-keuangan" class="jumper">Kementrian Keuangan</a>
+        <a href="#section-kementrian-luar-negeri" class="jumper">Kementrian Luar Negeri</a>
+        <a href="#section-kementrian-dalam-negeri" class="jumper">Kementrian Dalam Negeri</a>
+        <a href="#section-kementrian-olahraga" class="jumper">Kementrian Olahraga</a>
+        <a href="#section-kementrian-sosebud" class="jumper">Kementrian Sosial Seni & Budaya</a>
+        <a href="#section-kementrian-ti" class="jumper">Kementrian Teknik Informasi</a>
     </div>
 </div>
 @stop
