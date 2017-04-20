902484b78d0c5ec07df793c4d0c9fd6339285e3b
rere <rere@prismapp.io>
2017-04-20 16:00:52 +0700

    add template

--- resources/views/profil/profil.blade.php
+++ resources/views/profil/profil.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.master')
 
-@section('title','Beranda')
+@section('title','Profil')
 
 @section('content')
-
+<div class="col-md-12">
+    asd
+</div>
+<div class="col-md-10 col-md-offset-1">
+    <div class="col-md-9 border">
+        <h1 class="primary-color">Profil</h1>
+    </div>
+    <div class="col-md-3 border">
+        <h1 class="primary-color">Menu</h1>
+    </div>
+</div>
 @stop
